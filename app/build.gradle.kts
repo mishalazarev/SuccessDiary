b760a101ba858835f3c4302b22abbd342ab5138c
mishalazarev <mishalazav1998@gmail.com>
2025-10-29 17:43:31 +0300

    version 0.0.1 start commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,89 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.devtools)
+    alias(libs.plugins.dagger.hilt)
+}
+
+android {
+    namespace = "white.ball.success_diary"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        applicationId = "white.ball.success_diary"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    // Test
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    testImplementation(libs.androidx.room.testing)
+    // Room
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+    // Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+    // Navigation
+    implementation(libs.androidx.navigation.compose)
+    // coil
+    implementation(libs.coil.compose)
+    // gridLayout
+    implementation(libs.androidx.gridlayout)
+    // gson
+    implementation(libs.gson)
+    // workManager
+    implementation(libs.androidx.work.runtime.ktx)
+    // coroutine
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+    // viewModel
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    // Icons
+    implementation(libs.androidx.compose.material.icons.extended)
+}
---
+++ app/src/main/java/white/ball/success_diary/platform/app/SuccessDiaryApp.kt
@@ -0,0 +1,7 @@
+package white.ball.success_diary.platform.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SuccessDiaryApp : Application()
